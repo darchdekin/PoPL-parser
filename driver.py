805eec48ae8f5913a2dc39d720af45a26c51a7f0
Zoe Spriggs <82968887+zspriggs@users.noreply.github.com>
2023-11-08 12:44:22 -0600

    start of driver for testing
    I think this would be one way to test the grammar (maybe use a different way)

---
+++ driver.py
@@ -0,0 +1,22 @@
+import sys
+from io import StringIO
+from antlr4 import *
+from ExprLexer import ExprLexer
+from ExprParser import ExprParser
+
+#Coded with support from provided class files from Dr. Ufukepte
+
+def main():  
+    inputFile = open("project_deliverable_1_testcase.py", "r")
+	for x in inputFile:
+		s = InputStream(x)
+		lexer = ExprLexer(s)
+		stream = CommonTokenStream(lexer)
+		parser = ExprParser(stream)
+		tree = parser.prog()
+		if parser.getNumberOfSyntaxErrors() > 0:
+			print("failed: " + s)
+		else:
+			print("passed!")
+
+main()
